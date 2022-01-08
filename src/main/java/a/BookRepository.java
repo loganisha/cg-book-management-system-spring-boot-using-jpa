83572439194b46607ba4b4f7d31f9dc6c8f3d3fb
loganisha <69141688+loganisha@users.noreply.github.com>
2022-01-08 10:59:58 +0530

    Add files via upload

---
+++ src/main/java/a/BookRepository.java
@@ -0,0 +1,11 @@
+/*
+ * @Author nisha 
+ * JPA Repository are avaliable here
+ */
+package a;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+
+}
